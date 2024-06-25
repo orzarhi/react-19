2129766071ee47e8f63199ce924fb28fc4dbdef9
orzarhi <or.zarhi11@gmail.com>
2024-06-25 15:45:33 +0300

    first commit

---
+++ api-server/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+
+const todos = [
+    { id: 1, text: "Learn Vite 2" },
+    { id: 2, text: "Learn React" },
+];
+
+app.get('/api/todos', (req, res) => {
+    res.json(todos)
+});
+
+app.post('/api/todos', (req, res) => {
+    setTimeout(() => {
+        const body = req.body || {};
+        const todo = {
+            id: todos.length + 1,
+            text: body.text ?? '',
+        }
+
+        todos.push(todo);
+        res.json(todo);
+    }, 3000)
+})
+
+app.listen(8080, () => {
+    console.log('Server is running on port 8080');
+})
---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState, useOptimistic, useTransition } from "react"
+
+const getTodos = async () => {
+  const response = await fetch('http://localhost:8080/api/todos')
+
+  return response.json()
+}
+
+const addTodo = async (text) => {
+  const response = await fetch('http://localhost:8080/api/todos', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ text })
+  })
+
+  if (!response.ok) throw new Error('Failed to add todo')
+
+  return response.json()
+}
+
+export const App = () => {
+  const [todos, setTodos] = useState([])
+  const [newTodo, setNewTodo] = useState('')
+
+  const [optimisticTodos, setOptimisticTodos] = useOptimistic(todos)
+  const [isPending, startTransition] = useTransition()
+
+  const addNewTodo = async () => {
+    if (!newTodo) return;
+
+    setOptimisticTodos((todos) => [
+      ...todos,
+      { id: Date.now(), text: newTodo }
+    ])
+
+
+    await addTodo(newTodo)
+    setTodos(await getTodos())
+    setNewTodo('')
+
+  }
+
+
+  useEffect(() => { getTodos().then(setTodos) }, [])
+
+  return (
+    <>
+      <ul>
+        {optimisticTodos.map(todo => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
+      <div>
+        <input
+          type="text"
+          placeholder="New todo"
+          value={newTodo}
+          onChange={({ target }) => setNewTodo(target.value)}
+          onKeyUp={({ key }) => {
+            if (key === 'Enter') startTransition(() => addNewTodo())
+          }}
+        />
+      </div>
+    </>
+  )
+}
+
